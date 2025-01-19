adacaa3f562a3ef46235f8995b9f4d7d1c517aee
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2025-01-19 18:53:19 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,11 @@ input.onButtonPressed(Button.A, function () {
 	
 })
 leds16x8.init_leds()
-let img = 0
+let img = leds16x8.creer_image_vide()
+leds16x8.definir_pixel(
+img,
+1,
+1,
+true
+)
+leds16x8.montrer_grande_image(img)
