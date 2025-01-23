1d874da2235ccec41786b4add1506500f21f12a2
BuildTools <unconfigured@null.spigotmc.org>
2025-01-23 23:08:45 +0100

    ✅ Added test file

--- test.ts
+++ test.ts
@@ -1,1 +1,16 @@
-// les tests vont ici ; cela ne sera pas compilé si ce paquet est utilisé en tant qu'extension.
+let img2: Image = null
+let img = leds16x8.createImage(`
+    . . # . . . . . . . # . . . # .
+    . # # # . . . . . # # # . # # #
+    # # . # # . # . # # # # # # # #
+    # . . . # # # # # . . . # . . .
+    # . . . # . . . # . . . # . . .
+    # . . . # . . . # . . . # . . .
+    # . . . # . . . # . . . # . . .
+    # # # # # # # # # . . . # . . .
+    `)
+basic.forever(function () {
+    img2 = leds16x8.cloneImage(img)
+    leds16x8.setPixel(img2, 2, 2, true)
+    leds16x8.showImage(img2)
+})
